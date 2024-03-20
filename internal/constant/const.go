bdd1a1795bf5d7d40c1f03774f7cfd171631168b
dark-person <66205094+dark-person@users.noreply.github.com>
2024-03-20 20:09:20 +0800

    Add constant package to store common value

---
+++ internal/constant/const.go
@@ -0,0 +1,23 @@
+package constant
+
+// This file will stored the default file location for
+// 1. config.yaml
+// 2. database file
+
+// Root Directory to storing all config files.
+const RootDir = "gui-comicinfo"
+
+// Config filename, without extension. Used by `viper`.
+const ConfigName = "config"
+
+// Config file type. Used by `viper`.
+const ConfigType = "yaml"
+
+// Config filename, with extension. Used to check file existence.
+const ConfigFile = ConfigName + "." + ConfigType
+
+// Database filename.
+const DatabaseFile = "storage.db"
+
+// Database type.
+const DatabaseType = "sqlite3"
