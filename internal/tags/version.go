4a20e7b64ca0e0cbefea4d5139e8efa8c08c144b
dark-person <66205094+dark-person@users.noreply.github.com>
2024-03-23 17:55:37 +0800

    [Rebase] tidy up error in version

--- internal/tags/version.go
+++ internal/tags/version.go
@@ -7,6 +7,16 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Error represents program is outdated
+var ErrProgramOutdated = fmt.Errorf("outdated program")
+
+// Error represents database schema is outdated
+var ErrSchemaOutdated = fmt.Errorf("outdated schema")
+
+// Error represents database user_version is invalid,
+// usually appear in empty database
+var ErrInvalidVersion = fmt.Errorf("invalid user_version")
+
 // Latest Version of schema supported.
 //
 // Every database schema changes,
@@ -20,7 +30,7 @@ const LatestSchema = 1
 func checkVer(db *sql.DB) error {
 	row := db.QueryRow("PRAGMA user_version")
 	if row == nil {
-		return fmt.Errorf("empty user_version")
+		return ErrInvalidVersion
 	}
 
 	var userVersion int
@@ -31,16 +41,22 @@ func checkVer(db *sql.DB) error {
 
 	// Check schema version
 	logrus.Infof("DB User version: %d; Latest Support:%d", userVersion, LatestSchema)
+
+	if userVersion == 0 {
+		logrus.Error("invalid user_version")
+		return ErrInvalidVersion
+	}
+
 	if userVersion > LatestSchema {
 		logrus.Error("Program version is not support this version of database.")
-		return fmt.Errorf("outdated program")
+		return ErrProgramOutdated
 	}
 
 	if userVersion < LatestSchema {
 		logrus.Warn("Database version is outdated.")
 
 		// TODO: Migration scripts
-		return fmt.Errorf("outdated schema")
+		return ErrSchemaOutdated
 	}
 
 	logrus.Info("Database version is supported.")
